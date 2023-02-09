04eddce7d401bdd1d7241594829511cb0d25ce26
SebasAndres <sebastian.ignacio.andres@gmail.com>
2023-02-09 18:53:17 -0300

    api actualizacion

--- MyWallet - API/MyWalletApi/urls.py
+++ MyWallet - API/MyWalletApi/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("add_money/<user>/<psw>/<acc>/<value>/<det>/<cat>/<cuotes>/<interest>", views.add_money),
     path("spend_money/<user>/<psw>/<acc>/<value>/<det>/<cat>", views.spend_money_1c),
     path("spend_money/<user>/<psw>/<acc>/<value>/<det>/<cat>/<cuotes>/<interest>", views.spend_money),
-    path("account/<user_key>/<acc>", views.get_account)
+    path("account/<user_key>/<acc>", views.get_account),
+    path("view_ops/<user_key>/<psw>", views.view_ops)
 ]
